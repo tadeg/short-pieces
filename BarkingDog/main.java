f0040a2cd5cad9ff0985afa7f030b83a293282b6
tadeg <68877807+tadeg@users.noreply.github.com>
2020-08-19 14:47:00 +0200

    Update main.java
    Training excersise #1.01
    Content of main Class

--- BarkingDog/main.java
+++ BarkingDog/main.java
@@ -1,1 +1,7 @@
-//code
+public class main {
+
+    public static void main(String[] args) {
+        boolean result = BarkingDog.shouldWakeUp(true, -1);
+        System.out.println("The dog is barking and we should wake up = " +result);
+    }
+}
