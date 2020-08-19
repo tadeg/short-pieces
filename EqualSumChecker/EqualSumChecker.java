55dfa22493c282190ba73bfa4af2d6a6a2c6358d
tadeg <68877807+tadeg@users.noreply.github.com>
2020-08-19 17:55:06 +0200

    Create EqualSumChecker.java

---
+++ EqualSumChecker/EqualSumChecker.java
@@ -0,0 +1,12 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int nr1, int nr2, int nr3) {
+
+        if (nr1 + nr2 == nr3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
