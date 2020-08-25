a4488180c64f4eb286a43d822bc4360ee808295f
tadeg <68877807+tadeg@users.noreply.github.com>
2020-08-25 18:34:43 +0200

    Create main.java

---
+++ AreaCalculator/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        double areaCirc = AreaCalculator.area(4);
+        System.out.println(areaCirc);
+
+        double areaRect = AreaCalculator.area( 2,4);
+        System.out.println(areaRect);
+
+    }
+}
