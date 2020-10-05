206c31f3b770f8fc32d2017d635dff08d042e27b
tadeg <68877807+tadeg@users.noreply.github.com>
2020-10-05 23:12:45 +0200

    Update DayOfTheWeek.java

--- DayOfTheWeek/DayOfTheWeek.java
+++ DayOfTheWeek/DayOfTheWeek.java
@@ -1,4 +1,6 @@
 public class DayOfTheWeek {
+    
+    // The switch statement vs. if then else statement
 
     public static void printDayOfTheWeek (int day) {
 
@@ -25,7 +27,26 @@ public class DayOfTheWeek {
                 System.out.println("Invalid day");
                 break;
         }
-
     }
 
+    public static void printDayOfTheWeekAlt(int day) {
+
+        int dayNr = 1;
+        if (dayNr == 1) {
+            System.out.println("Monday");
+        } else if (dayNr == 2) {
+            System.out.println("Tuesday");
+        } else if (dayNr == 3) {
+            System.out.println("Wednesday");
+        } else if (dayNr == 4) {
+            System.out.println("Thursday");
+        } else if (dayNr == 5) {
+            System.out.println("Friday");
+        } else if (dayNr == 6) {
+            System.out.println("Saturday");
+        } else {
+            System.out.println("Invalid day");
+        }
+    }
 }
+
