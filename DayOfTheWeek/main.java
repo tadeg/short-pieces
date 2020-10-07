adeb73430775b1b9602fcea2b3d236e874f5d4de
tadeg <68877807+tadeg@users.noreply.github.com>
2020-10-07 21:29:22 +0200

    Update main.java

--- DayOfTheWeek/main.java
+++ DayOfTheWeek/main.java
@@ -1,7 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
+        DayOfTheWeek.printDayOfTheWeek(0);
+        DayOfTheWeek.printDayOfTheWeek(1);
+        DayOfTheWeek.printDayOfTheWeek(2);
+        DayOfTheWeek.printDayOfTheWeek(3);
+        DayOfTheWeek.printDayOfTheWeek(4);
         DayOfTheWeek.printDayOfTheWeek(5);
+        DayOfTheWeek.printDayOfTheWeek(6);
+        DayOfTheWeek.printDayOfTheWeek(7);
         DayOfTheWeek.printDayOfTheWeekAlt(1);
     }
 }
