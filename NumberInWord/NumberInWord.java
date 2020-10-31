b92c5ee58416989c764762075ef6b15221eac79b
tadeg <68877807+tadeg@users.noreply.github.com>
2020-10-31 13:51:38 +0100

    Update NumberInWord.java

--- NumberInWord/NumberInWord.java
+++ NumberInWord/NumberInWord.java
@@ -8,8 +8,6 @@ public class NumberInWord {
 
         public static void printNumberInWord(int number) {
 
-            int number = 1;
-
             switch (number) {
                 case 0:
                     System.out.println("ZERO");
@@ -51,26 +49,36 @@ public class NumberInWord {
 
         if (number == 0) {
         System.out.println("ZERO");
+        
         } else if (number == 1) {
         System.out.println("ONE");
+        
         } else if (number == 2) {
         System.out.println("TWO");
+        
         } else if (number == 3) {
         System.out.println("THREE");
+        
         } else if (number == 4) {
         System.out.println("FOUR");
+        
         } else if (number == 5) {
         System.out.println("FIVE");
+        
         } else if (number == 6) {
         System.out.println("SIX");
+        
         } else if (number == 7) {
         System.out.println("SEVEN");
+        
         } else if (number == 8) {
         System.out.println("EIGHT");
+        
         } else if (number == 9) {
         System.out.println("NINE");
+        
         } else {
-        System.out.println("OTHER");
+            System.out.println("OTHER");
     }
 
     }
